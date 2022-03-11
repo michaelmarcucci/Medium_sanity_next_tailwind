04ea1bd697774a55ed77af92861f268e9540b92c
michaelmarcucci <64811901+michaelmarcucci@users.noreply.github.com>
2022-03-10 23:19:42 -0500

    finished and ready to deploy

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,8 +1,19 @@
+import Link from 'next/link'
 import React from 'react'
+import { Post } from "../typings";
+
+interface Props {
+  posts: [Post];
+}
 
 function Posts() {
   return (
-    <div>Posts</div>
+    <div>
+      {posts.map(post => (
+        <Link key={post.} >
+        </Link>
+      ))}
+    </div>
   )
 }
 
--- sanity.js
+++ sanity.js
@@ -1,2 +1,15 @@
 import { createCurrentUserHook, createClient } from "next-sanity";
-import createImageUrlBuilder from "sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-10-21",
+  useCdn: process.env.NODE_ENV === "production"
+};
+
+export const sanityClient = createClient(config);
+// below image builder may cause problems due to ver diffs
+export const urlFor = (source) => imageUrlBuilder(config).image(source);
+
+export const useCurrentUser = createCurrentUserHook(config);
